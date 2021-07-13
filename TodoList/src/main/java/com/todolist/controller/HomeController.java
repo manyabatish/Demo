b9abddf35fc1faaebb977f1065d85774bbd72ae1
Manya <manyabatish99@gmail.com>
2021-07-13 16:45:23 +0530

    first commit

---
+++ TodoList/src/main/java/com/todolist/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.todolist.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Home Controller";
+	}
+
+}
